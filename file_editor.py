e20239ce90be0e4a9630d68fafae5b8a12cdb156
zero <50501099+augustusbd@users.noreply.github.com>
2020-05-02 22:07:25 -0500

    Update file_editor.py

--- file_editor.py
+++ file_editor.py
@@ -6,7 +6,7 @@ import shutil
 
 import string_functions as s
 
-# Edit A File
+# Edit A File - remove initially
 class FileEdit:
     def __init__(self, file_path):
         self.file = file_path
@@ -59,30 +59,41 @@ class FileEdit:
         return None
 
 
-# File Handler
+# File Handler - rename initially
 class FileHandler:
     def __init__(self):
         self.give_options()
 
 
     def give_options(self):
+        """Give user options for editing files."""
         choices = ['remove items', 'rename files']
         text = f"What would you like to do with the file? "
         choice = s.user_input_choice(text, choices)
+        
         if choice == 'remove items':
             self.remove_items()
+        
         elif choice == 'rename files':
             self.rename_files()
 
+        return None
+
     def rename_files(self):
-        folder_location = input("Enter file location of the files to rename: ")
+        folder_location = input("Enter fodler location of the files to be renamed: ")
         files = find_files(folder_location)
-        rename_template = input("Enter the way you want to rename the files: ")
-        new_location = input("Enter the new location of file. '.' for current directory: ")
+        rename_template = input("Enter the template for renaming the files: ")
+        new_location = input("Enter a folder location to send renamed files ('.' for current directory): ")
 
         # simple for now:
         # rename them to one name with the added "file number"
         rename_alphanumberical(files, rename_template, new_location)
+        return None
+
+    def remove_items(self):
+        """Remove items."""        
+        print('Removing Items is a work in progress.')
+        return None
 
 
 def rename_alphanumberical(files, new_name, new_location):
@@ -95,8 +106,29 @@ def rename_alphanumberical(files, new_name, new_location):
         filepath = os.path.join(new_location, new_name+add)
         shutil.move(files[x], filepath)
 
+    return None
 
 
+def rename_what():
+    """Ask user what to rename."""
+    print("Choose one of the following:")
+    print("\t1. Rename files using a template")
+    print("\t2. Change date styles to 'YEAR-MM-DD'")
+    choice = input("Choice: ")
+
+    if choice == 1:
+        print('rename template')
+
+    elif choice == 2:
+        rename_dates()
+
+    return None 
+
+
+def rename_dates():
+    """Change date styles to YEAR-MM-DD"""
+    files = find_files('.')
+    return None
 
 def find_files(directory, ext=None, exclude_string=None):
     """
